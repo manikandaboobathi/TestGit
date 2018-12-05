e6c7e70410771c18ee74506725410f43c729470a
manikandaboobathi <manikandan22795@gamil.com>
2018-12-05 13:49:25 -0800

    sucess

--- src/org/git/TestGit.java
+++ src/org/git/TestGit.java
@@ -3,6 +3,6 @@ package org.git;
 public class TestGit {
 public static void main(String[] args) {
 	System.out.println("sample");
-	System.out.println("test");
+	System.out.println("success");
 }
 }
